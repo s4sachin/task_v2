7cc7437c2292680bd43f5ebe74e6dc715d7f2d0a
s4sachin <sachinpurohit.text@gmail.com>
2023-04-03 20:18:58 +0530

    Interface for csv data

--- src/app/interface/data.ts
+++ src/app/interface/data.ts
@@ -1,2 +1,20 @@
 export interface Data {
+  policyID?: number;
+  statecode?: string;
+  county?: string;
+  eq_site_limit?: number;
+  hu_site_limit?: number;
+  fl_site_limit?: number;
+  fr_site_limit?: number;
+  tiv_2011?: number;
+  tiv_2012?: number;
+  eq_site_deductible?: number;
+  hu_site_deductible?: number;
+  fl_site_deductible?: number;
+  fr_site_deductible?: number;
+  point_latitude?: number;
+  point_longitude?: number;
+  line?: string;
+  construction?: string;
+  point_granularity?: number;
 }
