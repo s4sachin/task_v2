f6df6a40ac17fb32738f01df902c1d4e85a6045a
s4sachin <sachinpurohit.text@gmail.com>
2023-04-05 08:05:05 +0530

    Transform incoming text/csv data to Json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { TableComponent } from './component/table/table.component';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, TableComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [],
-  exports: [],
+  imports: [MatButtonModule, MatTableModule, MatToolbarModule, MatIconModule],
+  exports: [MatButtonModule, MatTableModule, MatToolbarModule, MatIconModule],
 })
 export class MaterialModule {}
