e1d63525973bb87ba7554d6525864f7d4bbe34f7
s4sachin <sachinpurohit.text@gmail.com>
2023-04-05 08:05:51 +0530

    Displaying data in Data Table

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Data } from '../interface/data';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-
-  constructor() { }
+  apiUrl =
+    'https://drive.google.com/file/d/1FeAI_D3D_xlfZ__a7MFSCviBLrVc2jFk/view?usp=share_link';
+  httpHeaders = [''];
+  constructor(private http: HttpClient) {}
+  getTableData(): Observable<Data[]> {
+    return this.http.get<Data[]>(this.apiUrl, {
+      headers: new HttpHeaders().set('API-Key', '<API-KEY>'),
+      responseType: 'text' as 'json',
+    });
+  }
 }
